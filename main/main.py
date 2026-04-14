48df7a4f8c675de29f69b787dba56c5286859e84
ripich102 <silverchariot12345678910@gmail.com>
2026-04-14 22:22:04 +0300

    added algorithm, project is fully done

--- main/main.py
+++ main/main.py
@@ -1,13 +1,15 @@
 import pygame
+import math
 from enum import Enum
 
 class Blocks(Enum):
-    AIR = 0,
-    WALL = 1,
-    START = 2,
+    AIR = 0
+    WALL = 1
+    START = 2
     END = 3
+    PATH = 4
 
-def setColor(color : pygame.Color, type : Blocks):
+def setColor(color : pygame.Color, type : Blocks) -> pygame.Color:
      if type == Blocks.AIR:
           color.update(221, 221, 221)
      elif type == Blocks.WALL:
@@ -16,16 +18,25 @@ def setColor(color : pygame.Color, type : Blocks):
           color.update(124, 252, 0)
      elif type == Blocks.END:
           color.update(255, 0, 0)
+     elif type == Blocks.PATH:
+          color.update(100, 100, 0)
      return color
 
+def getBlockPos(typelist : list, type : Blocks) -> tuple[int, int]:
+     for y in range(0, len(typelist)):
+          for x in range(0, len(typelist[y])):
+              if typelist[y][x][1] == type:
+                   return (x, y)
+     return -1, -1
+              
 def drawGrid(grid : list, render_surface : pygame.Surface) -> None :
      for y in range(0, len(grid)):
          for x in range(0, len(grid[y])):
-              type = type_array[y][x]
+              cell, type = grid[y][x]
               box_col = pygame.Color(0, 0, 0)
               setColor(box_col, type)
-              pygame.draw.rect(render_surface, box_col, grid[y][x])
-              pygame.draw.rect(render_surface, pygame.Color(0, 0, 0), grid[y][x], 2)
+              pygame.draw.rect(render_surface, box_col, cell)
+              pygame.draw.rect(render_surface, pygame.Color(0, 0, 0), cell, 2)
 
 def toIndexes(mx : int, my : int) -> tuple[int, int] :
      x = int(mx / 50) - 1
@@ -34,7 +45,78 @@ def toIndexes(mx : int, my : int) -> tuple[int, int] :
      if(x < GRID_SIZE_X and x >= 0 and y < GRID_SIZE_Y and y >= 0):
           return x, y
      else:
-          return -1, -1
+          return -1, -1     
+
+def get_g(a : tuple[int, int], b : tuple[int, int]) -> int:
+     x, y = a
+     xx, yy = b
+     g = 0
+     if y - 1 == yy:
+          if x == xx:
+               return 10
+     elif y  == yy:
+          return 10
+     elif y == yy + 1:
+          if x == xx:
+               return 10
+     return 14
+
+def heruistics(start : tuple[int, int], end : tuple[int, int]) -> int:
+     x, y = start
+     ex, ey = end
+     return int(math.sqrt((x - ex)**2 + (y - ey)**2)) * 10
+
+def neighbours(node : tuple[int, int], grid : list) -> list:
+     x, y = node
+     return [(x - 1, y - 1), (x - 0, y - 1), (x + 1, y - 1),
+             (x - 1, y - 0),                 (x + 1, y - 0),
+             (x - 1, y + 1), (x - 0, y + 1), (x + 1, y + 1)]
+
+def search_path(sx : int, sy : int, ex : int, ey : int, grid : list) -> list:
+     open = []
+     closed = []
+     open.append((sx, sy))
+
+     came_from = {}
+
+     while True:
+          current = None
+
+          best_f = 2147483647
+          for node in open:
+               x, y = node
+
+               g = get_g((sx, sy), node)
+               h = heruistics(node, (ex, ey))
+               f = g + h
+               if f < best_f :
+                   best_f = f
+                   current = node
+          if current not in open:
+               return [(-1, -1)]
+          open.remove(current)
+          closed.append(current)
+
+          if current == (ex, ey):
+               path = []
+               while current in came_from:
+                    path.append(current)
+                    current = came_from[current]
+               path.append((sx, sy))
+               path.reverse()
+               return path
+          
+          for node in neighbours(current, grid):
+               x, y = node
+               if(y >= 0 and y < len(grid)):
+                    if (x >= 0 and x < len(grid[y])):
+                         if grid[y][x][1] == Blocks.WALL or (x, y) in closed:
+                              continue
+                         else:
+                              if not node in open:
+                                   came_from[node] = current
+                                   open.append(node)
+     return [(-1, -1)]  
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
@@ -49,15 +131,14 @@ selected_type = Blocks.WALL
 GRID_SIZE_Y = 10
 GRID_SIZE_X = 10
 
+lines = 0
+
 grid = []
-type_array = []
 for j in range(0, GRID_SIZE_Y):
-        grid.append([])
-        type_array.append([])
-        for i in range(0, GRID_SIZE_X):
-            rectsize = 50
-            type_array[j].append(Blocks.AIR)
-            grid[j].append(pygame.Rect(50 + i * rectsize, 50 + j * rectsize, rectsize, rectsize))
+     grid.append([])
+     for i in range(0, GRID_SIZE_X):
+          rectsize = 50
+          grid[j].append([pygame.Rect(50 + i * rectsize, 50 + j * rectsize, rectsize, rectsize), Blocks.AIR])
 
 
 while running:
@@ -75,6 +156,23 @@ while running:
                   selected_type = Blocks.START
              elif event.key == pygame.K_4: 
                   selected_type = Blocks.END
+             elif event.key == pygame.K_r:
+                  if startExists and endExists:    
+
+                    for i in range(0, len(grid)):
+                         for j in range(0, len(grid[i])):
+                              if grid[i][j][1] == Blocks.PATH:
+                                   grid[i][j][1] = Blocks.AIR
+
+                    startx, starty = getBlockPos(grid, Blocks.START)
+                    endx, endy = getBlockPos(grid, Blocks.END)
+                    lines = search_path(startx, starty, endx, endy, grid)
+                    
+                    if lines[0] != (-1, -1):
+                         for x, y in lines:
+                              if grid[y][x][1] != Blocks.START and grid[y][x][1] != Blocks.END:
+                                   grid[y][x][1] = Blocks.PATH
+
     screen.fill("blue")     
     
     if clicked:
@@ -84,19 +182,25 @@ while running:
               flag = False
               if(startExists and selected_type == Blocks.START or (endExists and selected_type == Blocks.END)):
                    flag = True
-              elif (not startExists and selected_type == Blocks.START):
+
+              pos, type = grid[y][x]
+
+              if (type == Blocks.START):
+                   if (selected_type != Blocks.START):
+                        if not (selected_type == Blocks.END and endExists):
+                              startExists = False
+              if (type == Blocks.END):
+                   if (selected_type != Blocks.END):
+                        if not (selected_type == Blocks.START and startExists):
+                              endExists = False
+
+              if (not startExists and selected_type == Blocks.START):
                    startExists = True
               elif (not endExists and selected_type == Blocks.END):
-                   endExists = True
+                   endExists = True 
 
-              if (type_array[y][x] == Blocks.START or type_array[y][x] == Blocks.END):
-                   if (selected_type != Blocks.START):
-                        startExists = False
-                   if (selected_type != Blocks.END):
-                        endExists = False
               if (not flag):
-                   type_array[y][x] = selected_type
-              
+                   grid[y][x][1] = selected_type
 
     drawGrid(grid, screen)
 
